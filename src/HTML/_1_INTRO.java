9d19403a59cdf9d3b1ed7a96b0bfbf0c8d322654
Shiva Parasuram R <shivaparasuramr@gmail.com>
2026-01-21 22:30:44 +0530

    first commit

---
+++ src/HTML/_1_INTRO.java
@@ -0,0 +1,31 @@
+package HTML;
+/*
+
+HTML - markup language
+
+h1 to h6 -- size
+HR -- for a line
+br -- new line
+
+href -- reference link
+target= "Blank" -- open in new tab      <a href=https://www.youtube.com/" target="_blank">Youtube !</a>
+
+ */
+
+
+
+/*
+
+
+inline CSS -- for one line -- same file
+internal CSS -- apply from header -- apply or all para --
+external CSS --
+
+
+id ="submit" -- for button use id tag colors
+
+
+ */
+public class _1_INTRO {
+}
+
